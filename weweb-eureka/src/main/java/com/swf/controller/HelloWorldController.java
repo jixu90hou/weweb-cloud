faad2c63e5078caece8e89daceb0b6fdcf3d02c3
[jackshen] <[736201041@qq.com]]>
2018-06-16 18:48:37 +0800

    Initial commit

---
+++ weweb-eureka/src/main/java/com/swf/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.swf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!!!";
+    }
+}
