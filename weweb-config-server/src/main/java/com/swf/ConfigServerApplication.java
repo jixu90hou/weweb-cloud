79d388cd6a2a69481f4f0e2bec5d4f174e7e6e88
[jackshen] <[736201041@qq.com]]>
2018-06-24 20:06:37 +0800

    修改config配置

--- weweb-config-server/src/main/java/com/swf/ConfigServerApplication.java
+++ weweb-config-server/src/main/java/com/swf/ConfigServerApplication.java
@@ -1,10 +1,10 @@
 package com.swf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableAutoConfiguration
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
